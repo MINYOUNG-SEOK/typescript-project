9add5fdf06f0ba369af567d0f594a3f81e36c2f6
Rosie Seok <lovelyrosie731@gmail.com>
2025-06-06 01:16:11 +0900

    API 호출

--- src/configs/authConfig.ts
+++ src/configs/authConfig.ts
@@ -1,6 +1,2 @@
 export const clientSecret = process.env.REACT_APP_SPOTIFY_SECRET_ID;
 export const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-
-if (!clientId || !clientSecret) {
-    console.warn("❗ 클라이언트 ID 또는 시크릿이 누락되었습니다");
-}
